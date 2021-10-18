f221b60ff8cf871afd9e1ebcfde864e7500b9278
dmitriysemenko <dm123semenko@gmail.com>
2021-10-18 23:29:46 +0500

    update

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post("/", (req, res) => {
-    res.send("Register a user");
-});
+router.post(
+    "/",
+    body("email").isEmail(),
+    check("password")
+        .isLength({ min: 6 })
+        .withMessage("Please enter a password with 6 or more characters"),
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send("passed");
+    }
+);
 
 module.exports = router;
